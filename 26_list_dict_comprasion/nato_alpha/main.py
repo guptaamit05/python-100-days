f322743c4cf1f1c97bcb76666277aa3eb457919e
amitkc.gupta <amitkc.gupta@infobeans.com>
2026-08-11 16:33:16 +0530

    added try exept for handling errors in nato game.

--- 26_list_dict_comprasion/nato_alpha/main.py
+++ 26_list_dict_comprasion/nato_alpha/main.py
@@ -5,8 +5,15 @@ df = pd.read_csv("./nato_phonetic_alphabet.csv")
 rows = {value.letter:value.code for (new_key,value) in df.iterrows()}
 # print(rows)
 
-user_input = input("enter any name: \n").upper()
-result = {x:rows[x] for x in user_input if x in rows.keys()}
+def generate_phonetic():
+    user_input = input("enter any name: \n").upper()
+    try:
+        # result = [rows[x] for x in user_input if x in rows.keys()]
+        result = [rows[x] for x in user_input]
+    except KeyError:
+        print("Sorry, only letters in alphabet please.")
+        generate_phonetic()        
+    else:
+        print(result)
 
-print(result)
-
+generate_phonetic()
