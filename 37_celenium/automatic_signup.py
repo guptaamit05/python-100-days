1c9775f69a314a3d3070720e32dbfd645fdf9875
amitkc.gupta <amitkc.gupta@infobeans.com>
2026-08-14 15:19:46 +0530

    updated param in automatic signup code.

--- 37_celenium/automatic_signup.py
+++ 37_celenium/automatic_signup.py
@@ -7,7 +7,7 @@ from selenium.webdriver.common.keys import Keys
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option('detach', True)
 
-driver = webdriver.Chrome(chrome_options)
+driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://appbrewery.github.io/fake-newsletter-signup/")
 
 
